36cf267bb35f4f66b207bbd5e915ea3f7f93c4af
Fabio Ortega <fabiolin57@gmail.com>
2025-05-31 11:15:20 -0500

    cambio de diseño de botones

--- src/components/ButtonAddToCart.jsx
+++ src/components/ButtonAddToCart.jsx
@@ -1,13 +1,21 @@
- // Adjust the import path as necessary
+// Adjust the import path as necessary
 import { useCartStore } from '../store/useCartStore';
 
 
 export default function ButtonAddToCart() {
     const addToCart = useCartStore((state) => state.addToCart);
 
-    return(
-        <button onClick={() => { console.log("Adding to cart");addToCart(1);}} class="bg-guacamole-f text-amber-950 font-bold border-4  border-black rounded-lg px-3 md:px-6 py-1 md:py-3 shadow-[4px_4px_0_0_#3E6102] hover:bg-guacamole-b active:translate-y-1 active:shadow-[0px_0px_0_0_#3E6102] transition-all text-sm md:text-xl">
-                Añadir al carrito + 🥑
-            </button>
+    return (
+        <button onClick={() => { addToCart(1); }} class="group relative inline-block focus:ring-3 focus:outline-hidden w-fit active:translate-y-1 active:shadow-[0px_0px_0_0_#3E6102] transition-all">
+            <span
+                class="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-guacamole-f transition-transform group-hover:translate-x-0 group-hover:translate-y-0 "
+            ></span>
+
+            <span
+                class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black uppercase "
+            >
+                Añadir al carrito 🥑
+            </span>
+        </button>
     )
 }
