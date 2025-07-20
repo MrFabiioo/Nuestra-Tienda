6f2f144173d27e993c9f12db6782ad76497c13dd
Fabio Ortega <fabiolin57@gmail.com>
2025-07-20 12:24:37 -0500

    agregados los sliders

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,7 @@
+import { getProductBySlug } from "./products/get-product-by-slug";
 import { getProductsByPage } from "./products/getProducts";
 
 export const server ={
-    getProductsByPage
+    getProductsByPage,
+    getProductBySlug,
 };
