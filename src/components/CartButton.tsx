475d1d4278154b6cbf61eb36f3758fba07b58fdf
Fabio Ortega <fabiolin57@gmail.com>
2026-03-31 17:49:39 -0500

    feat(Checkout): actualizar navegación a la página de pago y eliminar página de carrito

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -176,7 +176,7 @@ export default function CartButton() {
               {/* Checkout Button */}
               <a
                 class="group relative focus:ring-3 focus:outline-hidden"
-                href="/cart"
+                href="/checkout"
               >
                 <span class="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-guacamole-f transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
                 <span class="relative w-full inline-block border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black uppercase text-center">
